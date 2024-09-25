f817fd056e74443af62df4d5bea9b72b502cbdce
Aaron Lao <91438480+aajmlao@users.noreply.github.com>
2024-09-25 12:46:32 -0700

    Project 1

--- server/routes/teams.js
+++ server/routes/teams.js
@@ -8,16 +8,12 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(`dirname ${__dirname}`)
-console.log(`filename: ${__filename}`)
-
-
 router.get('/', (req, res) => {
     res.status(200).json(NBATeams)
 })
 
-// router.get("/:teamID", (req, res) =>{
-//     res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
-// })
+router.get("/:teamID", (req, res) =>{
+    res.status(200).sendFile(path.resolve(__dirname, '../public/team.html'))
+})
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -4,11 +4,17 @@ const app = express()
 
 app.use('/public', express.static('./public'))
 app.use('/scripts', express.static('./public/scripts'))
-app.use("/teams",router)
 
-// app.get('/', (req, res) => {
-//     res.status(200).send('testing')
-// })
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">NBA Teams API</h1>')
+})
+
+app.use('/teams', router)
+
+app.get("*", (req, res) => {
+  res.status(404).sendFile('public/404.html');
+})
 
 const PORT = process.env.PORT || 3001
     
